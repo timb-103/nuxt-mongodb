f355d6f6d6634f30f01df8b00ae7935caf87a7c3
timb-103 <tim103@pm.me>
2023-07-25 10:33:38 +1000

    Added types to DB connection. Updated readme.

--- server/utils/mongo.ts
+++ server/utils/mongo.ts
@@ -1,17 +1,21 @@
-import { MongoClient } from "mongodb"
+import { MongoClient, Db } from "mongodb"
 
-let dbConnection = {} as any
+let connection: Db | null = null
 let connecting = true
 let connected = false
 
 export const mongo = {
   db() {
+    if (connection) {
+      return connection
+    }
+
     if (!connected && !connecting) {
-      console.log("No mongo connection, trying to reconnect...")
       connectToMongo()
     }
 
-    return dbConnection
+    console.log("No mongoDB connection, trying to reconnect...")
+    throw new Error("No mongoDB connection, trying to reconnect...")
   },
   connected() {
     return connected
@@ -19,30 +23,35 @@ export const mongo = {
 }
 
 export async function connectToMongo() {
-  console.log("Connecting to mongo database...")
+  console.log("Connecting to mongoDB...")
   connecting = true
 
   // create mongo client
   const client = new MongoClient(useRuntimeConfig().MONGO_CONNECTION_STRING)
 
-  // handle closing events
-  function handleEventClosed(eventName: string, event: object) {
-    console.log(`received ${eventName}: ${JSON.stringify(event, null, 2)}`)
-    process.exit(0)
-  }
-
   client.on("serverClosed", (event: object) => handleEventClosed("serverClosed", event))
   client.on("topologyClosed", (event: object) => handleEventClosed("topologyClosed", event))
 
-  // connect
   try {
+    // connect
     await client.connect()
-    dbConnection = client.db(useRuntimeConfig().MONGO_DB)
+
+    // create a connection to the db from env
+    connection = client.db(useRuntimeConfig().MONGO_DB)
+
+    // set to connected
     connected = true
-    console.log("Connected to mongo database.")
+
+    console.log("Connected to mongoDB.")
   } catch (e) {
-    console.log("Failed", e)
+    console.log("Failed to connect to mongoDB", e)
   }
 
   connecting = false
+}
+
+// handle closing events
+function handleEventClosed(eventName: string, event: object) {
+  console.log(`received ${eventName}: ${JSON.stringify(event, null, 2)}`)
+  process.exit(0)
 }
